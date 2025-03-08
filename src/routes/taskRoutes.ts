cae07c569ca07df01147f3fe64ab15502ffdf17c
willians.dev <samukawsouza@gmail.com>
2025-03-08 20:56:24 -0300

    refactor: index to routes

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -4,13 +4,16 @@ import { TaskController } from "../controllers/taskController";
 const taskRoutes = (controller: TaskController) => {
   const router = Router();
 
-  router.post("/tasks", controller.createTask);
-  router.get("/tasks", controller.findAllTasks);
-  router.get("/tasks/:id", controller.findTaskById);
-  router.put("/tasks/:id", controller.updateTask);
-  router.delete("/tasks/:id", controller.deleteTask);
-  router.get("/tasks/status/:status", controller.findByStatus);
-  router.patch("/tasks/:id/complete", controller.updateToComplete);
+  router.post("/tasks", controller.createTask.bind(controller));
+  router.get("/tasks", controller.findAllTasks.bind(controller));
+  router.get("/tasks/:id", controller.findTaskById.bind(controller));
+  router.put("/tasks/:id", controller.updateTask.bind(controller));
+  router.delete("/tasks/:id", controller.deleteTask.bind(controller));
+  router.get("/tasks/status/:status", controller.findByStatus.bind(controller));
+  router.patch(
+    "/tasks/:id/complete",
+    controller.updateToComplete.bind(controller)
+  );
 
   return router;
 };
