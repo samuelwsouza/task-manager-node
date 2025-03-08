3f21f2d48e70018f86b08369ed0c974ec9ae7060
willians.dev <samukawsouza@gmail.com>
2025-03-08 20:56:24 -0300

    refactor: index to routes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import taskRoutes from "./routes/taskRoutes";
-import TaskController from "./controllers/taskController";
+import { TaskController } from "./controllers/taskController";
+import { TaskService } from "./services/taskService";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const taskController = new TaskController();
+const taskService = new TaskService();
+const taskController = new TaskController(taskService);
 
 const router = taskRoutes(taskController);
 
 app.use(express.json());
-app.use(router);
+app.use("/api", taskRoutes(taskController));
 
 app.listen(PORT, () => console.log("Servidor funcionando..."));
